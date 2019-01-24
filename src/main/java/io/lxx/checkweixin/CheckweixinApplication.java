6c4f958afae4a63a8669a67814f2731cccbd0518
lxx <853555374@qq.com>
2019-01-24 20:25:43 +0800

    first init

---
+++ src/main/java/io/lxx/checkweixin/CheckweixinApplication.java
@@ -0,0 +1,14 @@
+package io.lxx.checkweixin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckweixinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CheckweixinApplication.class, args);
+    }
+
+}
+
